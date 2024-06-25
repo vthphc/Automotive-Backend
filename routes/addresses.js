4b3947eba2962419e4136a8f7cad9c2b93b00476
tkj3nfuq <thinphcv5@gmail.com>
2024-06-25 20:33:31 +0700

    addresses routes updated

--- routes/addresses.js
+++ routes/addresses.js
@@ -3,9 +3,28 @@ const router = express.Router();
 const Address = require('../models/addresses.js');
 
 router.get('/', async (req, res) => {
+    if (req.query.ids) {
+        const ids = req.query.ids.split(',');
+        try {
+            const addresses = await Address.find({ _id: { $in: ids } });
+            res.json(addresses);
+        } catch (err) {
+            res.json({ message: err });
+        }
+    } else {
+        try {
+            const addresses = await Address.find();
+            res.json(addresses);
+        } catch (err) {
+            res.json({ message: err });
+        }
+    }
+});
+
+router.get('/:addressId', async (req, res) => {
     try {
-        const addresses = await Address.find();
-        res.json(addresses);
+        const address = await Address.findById(req.params.addressId);
+        res.json(address);
     } catch (err) {
         res.json({ message: err });
     }
